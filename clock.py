3a63cbc55a4d86a8677e0b51603c71c351b62653
fcocasa <franciscocasarotti1994@gmail.com>
2021-10-22 18:37:06 -0300

    corrigiendo clock.py

--- clock.py
+++ clock.py
@@ -1,11 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import app
+from app import report_elements
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    app.report_elements()
+    report_elements()
 
 
 sched.start()
